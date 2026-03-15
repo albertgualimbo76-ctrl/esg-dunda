8f605ddc0006831094990c2b38d9f41a8df1cc59
albertgualimbo76-ctrl <albertgualimbo76@gmail.com>
2026-03-15 08:45:32 +0200

    Add files via upload

--- routers/pages/ass_direccao.py
+++ routers/pages/ass_direccao.py
@@ -1,17 +1,13 @@
-import os
+# routers/pages/ass_direccao.py
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-# Verifica o diretório de trabalho atual (adicione isso para depuração)
-print("Caminho de trabalho:", os.getcwd())  # Verifica o diretório onde o servidor está sendo executado
-
-# Usando caminho absoluto para garantir que o Render encontre o diretório corretamente
-templates = Jinja2Templates(directory=os.path.join(os.getcwd(), "templates"))
-
 router = APIRouter()
+
+templates = Jinja2Templates(directory="templates")
 
 @router.get("/ass_direccao", response_class=HTMLResponse)
 async def ass_direccao(request: Request):
-    # Retorna o template correto
     return templates.TemplateResponse("ass_direccao.html", {"request": request})
